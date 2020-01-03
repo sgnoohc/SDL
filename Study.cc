64db850ff82ee65cff67418569a722d77d244a60
Balaji Sathia Narayanan <balajivenkat96@gmail.com>
2020-01-03 12:48:55 -0800

    screw my last commit

--- Study.cc
+++
@@ -1,4 +0,0 @@
-#include "Study.h"
-
-void Study::bookStudy() {} // To be overloaded
-void Study::doStudy(SDL::Event& recoevent, std::vector<std::tuple<unsigned int, SDL::Event*>> simtrkevents) {} // To be overloaded
--- rooutil/looper.cc
+++
@@ -1,5 +0,0 @@
-//  .
-// ..: P. Chang, philip@physics.ucsd.edu
-
-//#include "cpptqdm/tqdm.h"
-#include "looper.h"
